db2225e2ceefaff5c2c93d805d42e16dacf20428
samuellee <samuel.lwy@live.com.sg>
2020-12-22 17:09:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Serverless Telegram Bot with AWS Lambda & API Gateway, Serverless Framework
+Note: These codes are part of the tutorial: [https://samleewy.com/blog/go-serverless-create-a-serverless-nodejs-telegram-bot-with-aws-lambda-api-gateway/](GO SERVERLESS: Create a serverless NodeJS Telegram Bot with AWS Lambda & API Gateway), for more info, please visit my blog!
+
+## How to run?
+1. Install NPM packages:
+`$ npm init`
+
+2. In `handler.js` (line 15), replace `<TELEGRAM_BOT_TOKEN>` with your Telegram Bot token.
+
+3. Deploy Serverless Framework
+`$ serverless deploy -v`
---
+++ handler.js
@@ -0,0 +1,36 @@
+'use strict';
+const axios = require('axios');
+
+module.exports.hello = async event => {
+  const telegramBody = JSON.parse(event.body);
+  const msg = telegramBody.message;
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  const regex = new RegExp(`/hello (.*)`);
+  const captureGroups = text.match(regex);
+  if (captureGroups && captureGroups.length > 1) {
+    const value = captureGroups[1];
+    try {
+      const res = await axios.post('https://api.telegram.org/bot<TELEGRAM_BOT_TOKEN>/sendMessage',
+      {
+        chat_id: chatId,
+        text: `Hello, ${value}!`
+      });
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ result: res.data })
+      }
+    } catch (err) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: err })
+      }
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ match: false })
+    }
+  }
+};
